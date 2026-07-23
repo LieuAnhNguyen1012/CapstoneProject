440305ccfa1cc36d18734061b0c7ecd78ca175ce
LieuNguyen <lieunguyen10122004@gmail.com>
2026-07-23 10:23:51 +0700

    banner faq project testimonials - add

---
+++ src/MasterWork/js/main.js
@@ -0,0 +1,45 @@
+const counters = document.querySelectorAll(".counter");
+const aboutStats = document.querySelector(".about-stats");
+
+function runCounter() {
+    counters.forEach(function (counter) {
+        const target = Number(counter.dataset.target);
+        let current = 0;
+        const step = Math.ceil(target / 100);
+
+        const timer = setInterval(function () {
+            current += step;
+
+            if (current >= target) {
+                current = target;
+                clearInterval(timer);
+            }
+
+            counter.innerText = current;
+        }, 20);
+    });
+}
+
+const observer = new IntersectionObserver(function (entries) {
+    if (entries[0].isIntersecting) {
+        runCounter();
+    } else {
+        counters.forEach(function (counter) {
+            counter.innerText = 0;
+        });
+    }
+});
+
+observer.observe(aboutStats);
+
+$(document).ready(function () {
+    $(".testimonials-carousel").owlCarousel({
+        items: 2,
+        margin: 30,
+        loop: true,
+        nav: false,
+        dots: true,
+        autoplay: false,
+        smartSpeed: 500
+    });
+});
